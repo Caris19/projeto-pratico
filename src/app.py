1538d0ef497a442448758bb2b456479531966e3b
Isabela Caris Fernandes da Silva <112494042+Caris19@users.noreply.github.com>
2022-11-21 23:45:34 -0300

    Add files via upload
    Segunda entrega

---
+++ src/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/contatos')
+def contatos():
+    return render_template('contatos.html')
+
+@app.route('/curriculo')
+def curriculo():
+    return render_template('curriculo.html')
+    
